6d6abc24736481e414ab1979ac0ceef75ba6e49d
gamesove132 <gamesove132@gmail.com>
2026-06-10 09:33:19 +0300

    Update NpcListener.java

--- src/main/java/me/auto/listeners/NpcListener.java
+++ src/main/java/me/auto/listeners/NpcListener.java
@@ -11,7 +11,7 @@ public class NpcListener implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractAtEntityEvent e) {
         if (!(e.getRightClicked() instanceof org.bukkit.entity.ArmorStand stand)) return;
-        Player p = (Player) e.getWhoClicked();
+        Player player = e.getPlayer();
         plugin.getNpcManager().onInteract(p, stand);
     }
 }
